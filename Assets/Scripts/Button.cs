89c73c8d818d750cc4f310e0df8f0b36ffd0d64c
Arjun Chauhan <arjunchauhan007@gmail.com>
2019-10-26 03:13:35 +0530

    Added Functionality

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -6,12 +6,17 @@ using TMPro;
 
 public class Button : MonoBehaviour
 {
-    int ButtonPressed=1;
-    int maxClicks = 0;
-    TextMeshProUGUI buttonText;
-    Image buttonImage;
+    [SerializeField]
+    private float lifetime = 0;
 
-    public void Start()
+    private int ButtonPressed;
+    private int maxClicks = 0;
+    private TextMeshProUGUI buttonText;
+    private Image buttonImage;
+
+    private float timer = 0;
+
+    private void Start()
     {
         buttonText = GetComponentInChildren<TextMeshProUGUI>();
         buttonImage = GetComponent<Image>();
@@ -22,19 +27,45 @@ public class Button : MonoBehaviour
         buttonText.text = ButtonPressed.ToString();
         buttonImage.color = Random.ColorHSV(0, 1, 1, 1, 0.5f, 1);
     }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+
+        if(timer > lifetime)
+        {
+            GameManager.instance.DecrementScore();
+            //Destroy(gameObject);
+        }
+    }
+
     public void ButtonClicked()
     {
+        timer = 0;
         ButtonPressed--;
 
         if (ButtonPressed > 0)
         {
             buttonText.text = ButtonPressed.ToString();
         }
+        else if(ButtonPressed < 0)
+        {
+            GameManager.instance.DecrementScore();
+            Destroy(gameObject);
+        }
         else
         {
-            gameObject.SetActive(false);
+            Color c = buttonImage.color;
+            c.a = 0;
+            buttonImage.color = c;
+            buttonText.gameObject.SetActive(false);
+            Destroy(gameObject, 0.3f);
         }
-
     }
 
+    private void OnDestroy()
+    {
+        if(ButtonPressed == 0 && timer < lifetime)
+            GameManager.instance.IncrementScore();
+    }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,24 +4,46 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-public class ButtonState
-{
-    public GameObject button;
-    public int Clicks;
-}
 public class GameManager : MonoBehaviour
-{ 
-    public GameObject ButtonPrefab;
-    public Transform canvas;
-    public List<ButtonState> AllButtons = new List<ButtonState>();
+{
+    public static GameManager instance = null;
+
+    [SerializeField]
+    private GameObject buttonPrefab;
+    [SerializeField]
+    private Transform canvas;
+    [SerializeField]
+    private TextMeshProUGUI scoreText;
 
     private float halfScreenWidth, halfScreenHeight;
+    private float buttonWidth;
+
+    private int score = 0;
+    private float buttonFrequency;
+    private int level;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         halfScreenHeight = Screen.height / 2;
         halfScreenWidth = Screen.width / 2;
+        buttonWidth = buttonPrefab.GetComponent<RectTransform>().rect.width / 2;
+        scoreText.text = score.ToString();
+        buttonFrequency = 2.0f;
+        level = 1;
+
         StartCoroutine("GenerateShapes");
     }
 
@@ -29,19 +51,31 @@ public class GameManager : MonoBehaviour
     {
         while (true)
         {
-            var Shape = Instantiate(ButtonPrefab, canvas) as GameObject;
-            float x = Random.Range(-0.9f * halfScreenWidth, 0.9f * halfScreenWidth);
-            float y = Random.Range(-0.9f * halfScreenHeight, 0.9f * halfScreenHeight);
+            var Shape = Instantiate(buttonPrefab, canvas) as GameObject;
+            float x = Random.Range(-halfScreenWidth + buttonWidth, halfScreenWidth - buttonWidth);
+            float y = Random.Range(-halfScreenHeight + buttonWidth, halfScreenHeight - buttonWidth);
             Vector2 pos = new Vector2(x, y);
             int Clicks = Random.Range(1, 5);
             Shape.GetComponent<RectTransform>().localPosition = pos;
             Shape.GetComponentInChildren<TextMeshProUGUI>().text = Clicks.ToString();
-            ButtonState obj = new ButtonState();
-            obj.button = Shape;
-            obj.Clicks = Clicks;
-            AllButtons.Add(obj);
-            yield return new WaitForSeconds(2.0f);
+            yield return new WaitForSeconds(buttonFrequency);
         }
+    }
+
+    public void IncrementScore()
+    {
+        scoreText.text = (++score).ToString();
+
+        if (score % 10 == 0 && score > 0)
+        {
+            buttonFrequency = Mathf.Max(0.25f, buttonFrequency - 0.2f);
+        }
+    }
+
+    public void DecrementScore()
+    {
+        score = Mathf.Max(0, --score);
+        scoreText.text = (score).ToString();
     }
 
 }
